54b56f59f4c6b151c692d2a20c8f5da1d46dc288
Angus Wilson <1103557w@student.gla.ac.uk>
2022-10-26 14:32:38 +0100

    removed testing numpy bits from test - and got skeletal patch class set up

---
+++ randomtesting/patch.py
@@ -0,0 +1,10 @@
+class Patch:
+    def __int__(self, status):
+        self.status = status
+
+    def event(self):
+        match self.status:
+            case 1:
+                self.status = 0
+            case 2:
+                self.status = 1
