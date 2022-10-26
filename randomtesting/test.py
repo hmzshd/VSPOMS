1513c3397e907897ad8cce7195717dbebf096400
Angus Wilson <1103557w@student.gla.ac.uk>
2022-10-26 12:58:39 +0100

    yup numpy much slower (factor of 14 i'd say) - surprising but there we go, will use inbuilt random

--- randomtesting/test.py
+++ randomtesting/test.py
@@ -2,17 +2,18 @@ import numpy as np
 import time
 import random
 
-
+n = 1000_000
+rng = np.random.default_rng()
 # probabilityList = [0.8, 0.2, 0.4,0.64]
 valuesList = [.8,.2,.4,.64]
 start_time = time.time()
-for i in range(1000):
-    print(np.random.choice(valuesList, 1, p=[0.3,0.25,0.05,0.4]))
+for i in range(n):
+    rng.choice(valuesList, 1, p=[0.3,0.25,0.05,0.4])
 
 npTime = time.time() - start_time
 start_time = time.time()
-for i in range(1000):
-    print(random.choices(valuesList, weights=[0.3,0.25,0.05,0.4], k=1)[0])
+for i in range(n):
+    random.choices(valuesList, weights=[0.3,0.25,0.05,0.4], k=1)
 
 randomTime = time.time() - start_time
-print(randomTime, npTime)
+print(f"inbuilt: {randomTime}, not: {npTime}")
