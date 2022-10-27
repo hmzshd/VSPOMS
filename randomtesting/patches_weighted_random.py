ded788994efec0f2cc9c4dd66bc4c5edc86116c3
Max Bell <2604835b@student.gla.ac.uk>
2022-10-27 18:13:51 +0100

    Added function to select patch weighted-randomly from list of patches

--- randomtesting/patches_weighted_random.py
+++ randomtesting/patches_weighted_random.py
@@ -1,4 +1,39 @@
 # function randomly selects patch for event based on internally stored weight
+import random
+from itertools import accumulate
+from bisect import bisect
+from patchGenerate import generatePatchListRandom
+
 
 def patches_weighted_random(patches):
+    n = len(patches)
 
+    cum_weights = []
+    for patch in patches:
+        cum_weights.append(patch.probability)
+    cum_weights = list(accumulate(cum_weights))
+
+    total = cum_weights[-1] + 0.0  # convert to float
+    if total <= 0.0:
+        raise ValueError('Total of weights must be greater than zero')
+
+    hi = n - 1
+    return patches[bisect(cum_weights, random.random() * total, 0, hi)]
+
+
+total = 0
+patch_list = generatePatchListRandom(5)
+for i in patch_list:
+
+
+selected_list =[]
+for i in range(100):
+    selected_list.append(patches_weighted_random(patch_list))
+
+counts = []
+for i in selected_list:
+    if [i.probability, selected_list.count(i), i] not in counts:
+        counts.append([i.probability, selected_list.count(i), i])
+
+for i in counts:
+    print(i)
