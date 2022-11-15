1bc3e71549807ca3aa3199e0cb2a3060e0c63a3a
Max Bell <2604835b@student.gla.ac.uk>
2022-11-15 21:40:06 +0000

    created quantity functions for dispersal_kernal, connectivity, colonization

--- randomtesting/patch.py
+++ randomtesting/patch.py
@@ -1,5 +1,6 @@
 from math import pi
 
+
 class Patch:
     def __init__(self, status, x, y, r):
         self.status = status
@@ -9,7 +10,6 @@ class Patch:
         self.radius = r
         self.area = pi * (r**2)
 
-
     def event(self):
         match self.status:
             case 1:
@@ -17,44 +17,34 @@ class Patch:
             case 2:
                 self.status = 1
 
-
     def set_probability(self, prob):
         self.probability = prob
 
-
-    def calculateProbability(self, probability):
-        self.probability = probability
-
+    def get_probability(self):
+        return self.probability
 
     def set_status(self, status):
         self.status = status
 
-
     def get_status(self):
         return self.status
-
 
     def set_coords(self, x, y):
         self.x_coord = x
         self.y_coord = y
 
-
     def get_coords(self):
-        return (self.x_coord, self.y_coord)
-
+        return self.x_coord, self.y_coord
 
     def set_radius(self, radius):
         self.radius = radius
         self.area = pi * (radius ** 2)
 
-
     def get_radius(self):
         return self.radius
 
-
     def get_area(self):
         return self.area
-
 
     def __str__(self):
         return f"Status: {self.status}, event probability: {self.probability}\n coordinates: {self.x_coord}, {self.y_coord}, radius: {self.radius} area: {self.area}\n"
---
+++ randomtesting/spom_quantities.py
@@ -0,0 +1,38 @@
+import math
+
+# SPOM quantity functions
+
+
+def dispersal_kernal(a, patch_i, patch_j):
+    # parameter a is the species-specific dispersal constant.
+
+    return math.exp((-a) * distance(patch_i, patch_j))
+
+
+def connectivity(patches, patch_i, a):
+    # MUST CLARIFY EXPONENT ON AREA FACTOR
+
+    connec = 0
+    for patch in patches:
+        if patch != patch_i:
+            connec += patch.get_probability() * dispersal_kernal(a, patch_i, patch) * patch.get_area()
+
+    return connec
+
+
+def colonization(patches, patch_i, a, y):
+    # y is species-specific constant
+    # MUST CLARIFY WHETHER y=a
+
+    return 1 - math.exp(-y * connectivity(patches, patch_i, a))
+
+
+def extinction():
+    pass
+
+
+def distance(patch_i, patch_j):
+    i_coords = patch_i.get_coords()
+    j_coords = patch_j.get_coords()
+
+    return math.sqrt((i_coords[0]-j_coords[0])**2 + (i_coords[1]-j_coords[1])**2)
