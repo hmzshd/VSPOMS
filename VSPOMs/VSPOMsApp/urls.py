c1b4e05c7970ceb2f44f13c5c41f455596e81e44
Timothy Wang <2556936w@student.gla.ac.uk>
2023-01-18 13:39:26 +0000

    Remove redundant pages and URL mappings

--- VSPOMs/VSPOMsApp/urls.py
+++ VSPOMs/VSPOMsApp/urls.py
@@ -4,9 +4,6 @@ from VSPOMsApp import views
 app_name = 'VSPOMs'
 
 urlpatterns = [
-    path('', views.simulate, name='simulate'),
-    path('create', views.create, name='create'),
     path('graphs', views.graphs, name='graphs'),
-    path('settings', views.settings, name='settings'),
     path('index', views.index, name='index'),
 ]
