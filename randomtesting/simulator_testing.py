02ad18f5c90ee54efd1c2f856478668e26e57c84
Max Bell <2604835b@student.gla.ac.uk>
2023-01-23 16:49:08 +0000

    Merged graph-data-return into main.

--- randomtesting/simulator_testing.py
+++ randomtesting/simulator_testing.py
@@ -4,5 +4,8 @@ from patch_generate import generate_patch_list_random
 from simulator import Simulator
 
 patch_list = generate_patch_list_random(30)
-spom_sim = Simulator(patch_list)
-spom_sim.simulate(100, 5)
+spom_sim = Simulator(patch_list, 60, 5)
+spom_sim.simulate()
+spom_sim.print_frame()
+print(spom_sim.get_turnovers())
+print(type(spom_sim.get_data()))
