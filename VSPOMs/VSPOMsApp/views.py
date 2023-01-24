f00410f0c2a3b6fe1f907d9cd923499dcc54ad48
Roger Luo <2572839l@student.gla.ac.uk>
2023-01-24 18:46:36 +0000

    graphs repetition removed

--- VSPOMs/VSPOMsApp/views.py
+++ VSPOMs/VSPOMsApp/views.py
@@ -44,118 +44,33 @@ def index(request):
     dfi['date'] = pd.to_datetime(dfi['date'])
     start = 12
     obs = len(dfi)
+    graphs = {'graph1':'','graph2':'','graph3':'','graph4':''}
 
-    # new datastructure for animation
-    df = pd.DataFrame()  # container for df with new datastructure
-    for i in np.arange(start, obs):
-        dfa = dfi.head(i).copy()
-        dfa['ix'] = i
-        df = pd.concat([df, dfa])
+    for graph in graphs.keys():
+            # new datastructure for animation
+        df = pd.DataFrame()  # container for df with new datastructure
+        for i in np.arange(start, obs):
+            dfa = dfi.head(i).copy()
+            dfa['ix'] = i
+            df = pd.concat([df, dfa])
 
-    # plotly figure
-    fig1 = px.line(df, x='date', y=['GOOG', 'AAPL', 'AMZN', 'FB', 'NFLX', 'MSFT'],
-                  animation_frame='ix',
-                  # template = 'plotly_dark',
-                  width=1000, height=600)
+        # plotly figure
+        fig = px.line(df, x='date', y=['GOOG', 'AAPL', 'AMZN', 'FB', 'NFLX', 'MSFT'],
+                    animation_frame='ix',
+                    # template = 'plotly_dark',
+                    width=1000, height=600)
 
-    # attribute adjusments
-    fig1.layout.updatemenus[0].buttons[0]['args'][1]['frame']['redraw'] = True
+        # attribute adjusments
+        fig.layout.updatemenus[0].buttons[0]['args'][1]['frame']['redraw'] = True
 
-    fig1.update_layout(
-        autosize=False,
-        width=500,
-        height=400,
-    )
-    graph1 = fig1.to_html(full_html=False)
+        fig.update_layout(
+            autosize=False,
+            width=500,
+            height=400,
+        )
+        graphs[graph] = fig.to_html(full_html=False)
 
-    # input data and start of third graph template
-    dfi = px.data.stocks().head(50)
-    dfi['date'] = pd.to_datetime(dfi['date'])
-    start = 12
-    obs = len(dfi)
-
-    # new datastructure for animation
-    df = pd.DataFrame()  # container for df with new datastructure
-    for i in np.arange(start, obs):
-        dfa = dfi.head(i).copy()
-        dfa['ix'] = i
-        df = pd.concat([df, dfa])
-
-    # plotly figure
-    fig2 = px.line(df, x='date', y=['GOOG', 'AAPL', 'AMZN', 'FB', 'NFLX', 'MSFT'],
-                  animation_frame='ix',
-                  # template = 'plotly_dark',
-                  width=1000, height=600)
-
-    # attribute adjusments
-    fig2.layout.updatemenus[0].buttons[0]['args'][1]['frame']['redraw'] = True
-
-    fig2.update_layout(
-        autosize=False,
-        width=500,
-        height=400,
-    )
-    graph2 = fig2.to_html(full_html=False)
-
-    # input data and start of first graph template
-    dfi = px.data.stocks().head(50)
-    dfi['date'] = pd.to_datetime(dfi['date'])
-    start = 12
-    obs = len(dfi)
-
-    # new datastructure for animation
-    df = pd.DataFrame()  # container for df with new datastructure
-    for i in np.arange(start, obs):
-        dfa = dfi.head(i).copy()
-        dfa['ix'] = i
-        df = pd.concat([df, dfa])
-
-    # plotly figure
-    fig3 = px.line(df, x='date', y=['GOOG', 'AAPL', 'AMZN', 'FB', 'NFLX', 'MSFT'],
-                  animation_frame='ix',
-                  # template = 'plotly_dark',
-                  width=1000, height=600)
-
-    # attribute adjusments
-    fig3.layout.updatemenus[0].buttons[0]['args'][1]['frame']['redraw'] = True
-
-    fig3.update_layout(
-        autosize=False,
-        width=500,
-        height=400,
-    )
-    graph3 = fig3.to_html(full_html=False)
-
-    # input data and start of fourth graph template
-    dfi = px.data.stocks().head(50)
-    dfi['date'] = pd.to_datetime(dfi['date'])
-    start = 12
-    obs = len(dfi)
-
-    # new datastructure for animation
-    df = pd.DataFrame()  # container for df with new datastructure
-    for i in np.arange(start, obs):
-        dfa = dfi.head(i).copy()
-        dfa['ix'] = i
-        df = pd.concat([df, dfa])
-
-    # plotly figure
-    fig4 = px.line(df, x='date', y=['GOOG', 'AAPL', 'AMZN', 'FB', 'NFLX', 'MSFT'],
-                  animation_frame='ix',
-                  # template = 'plotly_dark',
-                  width=1000, height=600)
-
-    # attribute adjusments
-    fig4.layout.updatemenus[0].buttons[0]['args'][1]['frame']['redraw'] = True
-
-    fig4.update_layout(
-        autosize=False,
-        width=500,
-        height=400,
-    )
-    graph4 = fig4.to_html(full_html=False)
-
-    graphs = {'map':''}
+    patch_map = {'map':''}
 
     p = figure(x_range=(-map_size//10, map_size*1.1), y_range=(0, map_size*1.1), tools=[],
            title='Point Draw Tool')
@@ -176,15 +91,15 @@ def index(request):
     p.add_tools(draw_tool)
     p.toolbar.active_tap = draw_tool
 
-    graphs['map'] = p
+    patch_map['map'] = p
 
-    script, div = components(graphs)
+    script, div = components(patch_map)
 
     context_dict = {}
     context_dict['script'] = script
     context_dict['bokeh_div'] = div
     context_dict['table'] = table
-    context_dict ['graphs'] = {'graph1': graph1, 'graph2': graph2, 'graph3': graph3, 'graph4': graph4}
+    context_dict ['graphs'] = graphs
 
     return render(request, 'VSPOMs/index.html', context=context_dict)
 
