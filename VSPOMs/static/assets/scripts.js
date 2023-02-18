050ad1b4b72e721572abec0233186dd0d3cf5a74
Timothy Wang <2556936w@student.gla.ac.uk>
2023-02-18 23:28:54 +0000

    Animation & page transition changes

--- VSPOMs/static/assets/scripts.js
+++ VSPOMs/static/assets/scripts.js
@@ -1,51 +1,53 @@
 $(document).ready(function(){
 
     // Initial page load
-    $("header").delay(4000).fadeIn(500);
+    $("#header-wrapper").delay(4000).fadeIn(500);
     $("#loading-panel").delay(3000).fadeOut(500);
-    $("#create-panel").hide(1000).delay(3000).show(500);
-    $("#simulate-panel").hide(1000);
-    $("#graphs-panel").hide(1000);
-    $("#settings-panel").hide(1000);
-
-
-    // Navigation buttons
-    function resetNav() {
-        $("#button-create, #button-simulate, #button-graphs, #button-settings").removeClass("active-page");
-        $("#create-panel, #simulate-panel, #graphs-panel, #settings-panel").hide(500);
-    };
+    $("#create-panel").fadeOut(1000).delay(3000).fadeIn(500);
+    $("#simulate-panel").fadeOut(1000);
+    $("#graphs-panel").fadeOut(1000);
+    $("#settings-panel").fadeOut(1000);
 
 
     // Navigation functions
+    function resetNav() {
+        $("#button-create, #button-simulate, #button-graphs, #button-settings").removeClass("active-page");
+        $("#create-panel, #simulate-panel, #graphs-panel, #settings-panel").hide(0);
+    };
+
     function openCreate() {
         if (!$("#button-create").hasClass("active-page")) {
             resetNav();
             $("#button-create").addClass("active-page");
-            $("#create-panel").show(500);
+            $("#create-panel").fadeIn(200);
         }
     };
+
     function openSimulate() {
         if (!$("#button-simulate").hasClass("active-page")) {
             resetNav();
             $("#button-simulate").addClass("active-page");
-            $("#simulate-panel").show(500);
+            $("#simulate-panel").fadeIn(200);
         }
     };
+
     function openGraphs() {
         if (!$("#button-graphs").hasClass("active-page")) {
             resetNav();
             $("#button-graphs").addClass("active-page");
-            $("#graphs-panel").show(500);
+            $("#graphs-panel").fadeIn(200);
         }
     };
+
     function openSettings() {
         if (!$("#button-settings").hasClass("active-page")) {
             resetNav();
             $("#button-settings").addClass("active-page");
-            $("#settings-panel").show(500);
+            $("#settings-panel").fadeIn(200);
         }
     };
     
+
     // Navigation button click events
     $("#button-create").click(function() {
         openCreate();
@@ -59,6 +61,7 @@ $(document).ready(function(){
     $("#button-settings").click(function() {
         openSettings();
     });
+
 
     // Navigation key events
     $(document).keydown(function(e) {
