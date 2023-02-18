6501a79d3c7cd59f7539f6449c45e6e59f16bfeb
Roger Luo <2572839l@student.gla.ac.uk>
2023-02-18 16:50:41 +0000

    data retraction minor logic fix 2

--- VSPOMs/static/javascripts/data_retraction.js
+++ VSPOMs/static/javascripts/data_retraction.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         var ds = Bokeh.documents[0].get_model_by_name('patch_data_source');
         $.ajax({
             type: "POST",
-            url: "/patches",
+            url: "vspoms/patches",
             data: ds,
             success: function (response){
                 $("#button-run").hide()
