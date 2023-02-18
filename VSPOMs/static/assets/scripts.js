72e805c44740420cab4bd1cc1be1d1e5d153c146
Timothy Wang <2556936w@student.gla.ac.uk>
2023-02-18 22:54:32 +0000

    Rename "Settings" to "Parameters", add arrow-key navigation

--- VSPOMs/static/assets/scripts.js
+++ VSPOMs/static/assets/scripts.js
@@ -8,34 +8,75 @@ $(document).ready(function(){
     $("#graphs-panel").hide(1000);
     $("#settings-panel").hide(1000);
 
+
     // Navigation buttons
     function resetNav() {
         $("#button-create, #button-simulate, #button-graphs, #button-settings").removeClass("active-page");
         $("#create-panel, #simulate-panel, #graphs-panel, #settings-panel").hide(500);
-    }
-    // Show create page
-    $("#button-create").click(function(){
-        if (!$(this).hasClass("active-page")) {
-            resetNav(); $("#button-create").addClass("active-page"); $("#create-panel").show(500);
+    };
+
+
+    // Navigation functions
+    function openCreate() {
+        if (!$("#button-create").hasClass("active-page")) {
+            resetNav();
+            $("#button-create").addClass("active-page");
+            $("#create-panel").show(500);
         }
-    });
-    // Show simulate page
-    $("#button-simulate").click(function(){
-        if (!$(this).hasClass("active-page")) {
-            resetNav(); $("#button-simulate").addClass("active-page"); $("#simulate-panel").show(500);
+    };
+    function openSimulate() {
+        if (!$("#button-simulate").hasClass("active-page")) {
+            resetNav();
+            $("#button-simulate").addClass("active-page");
+            $("#simulate-panel").show(500);
         }
-    });
-    // Show graphs page
-    $("#button-graphs").click(function(){
-        if (!$(this).hasClass("active-page")) {
-            resetNav(); $("#button-graphs").addClass("active-page"); $("#graphs-panel").show(500);
+    };
+    function openGraphs() {
+        if (!$("#button-graphs").hasClass("active-page")) {
+            resetNav();
+            $("#button-graphs").addClass("active-page");
+            $("#graphs-panel").show(500);
         }
-    });
-    // Show settings page
-    $("#button-settings").click(function(){
-        if (!$(this).hasClass("active-page")) {
-            resetNav(); $("#button-settings").addClass("active-page"); $("#settings-panel").show(500);
+    };
+    function openSettings() {
+        if (!$("#button-settings").hasClass("active-page")) {
+            resetNav();
+            $("#button-settings").addClass("active-page");
+            $("#settings-panel").show(500);
         }
+    };
+    
+    // Navigation button click events
+    $("#button-create").click(function() {
+        openCreate();
     });
+    $("#button-simulate").click(function() {
+        openSimulate();
+    });
+    $("#button-graphs").click(function() {
+        openGraphs();
+    });
+    $("#button-settings").click(function() {
+        openSettings();
+    });
+
+    // Navigation key events
+    $(document).keydown(function(e) {
+        switch(e.which) {
+        // Right arrow key
+        case 39:
+            if ($("#button-create").hasClass("active-page")) {openSettings()}
+            else if ($("#button-settings").hasClass("active-page")) {openSimulate()}
+            else if ($("#button-simulate").hasClass("active-page")) {openGraphs()};
+            break;
+        // Left arrow key
+        case 37:
+            if ($("#button-graphs").hasClass("active-page")) {openSimulate()}
+            else if ($("#button-simulate").hasClass("active-page")) {openSettings()}
+            else if ($("#button-settings").hasClass("active-page")) {openCreate()}
+            break;
+        };
+      });
+
 
 });
