4d29aa703f2ea164550dd0850c0a3e35b7d2e9ce
Angus Wilson <1103557w@student.gla.ac.uk>
2023-02-20 13:28:43 +0000

    enabled customisation of simulator object, and changed return of settings in csv parser
    changed things that were constants to variables set on creation of object. settings of parse_csv now returns a dictionary

--- VSPOMs/simulator/parser_test.py
+++ VSPOMs/simulator/parser_test.py
@@ -1,12 +1,12 @@
 from parser import parse_csv
-
+# from simulator import Simulator
 patch_list, settings = parse_csv("demo.csv")
 
 
-print(len(patch_list))
-for patch in patch_list:
-    print(patch)
+print(patch_list)
 print(settings)
+
+
 
 # @ property
 # also initialise list as list()
