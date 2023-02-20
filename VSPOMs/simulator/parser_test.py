cc5d0fca1ca6eecf10c8e2a2ec1623f391717529
Angus Wilson <1103557w@student.gla.ac.uk>
2023-02-20 13:34:43 +0000

    added testing file
    added testing file that should demonstrate how simulator should be created from now on

--- VSPOMs/simulator/parser_test.py
+++ VSPOMs/simulator/parser_test.py
@@ -1,12 +1,20 @@
 from parser import parse_csv
-# from simulator import Simulator
+from simulator import Simulator
+from simulator_testing import generate_patch_list_random
 patch_list, settings = parse_csv("demo.csv")
 
 
 print(patch_list)
 print(settings)
 
+sim = Simulator(patches=generate_patch_list_random(15), steps=50, replicates=5,
+                          dispersal_alpha=settings["dispersal_alpha"],
+                          area_exponent_b=settings["area_exponent_b"],
+                          species_specific_constant_y=settings["species_specific_constant_y"],
+                          species_specific_constant_u=settings["species_specific_constant_u"],
+                          patch_area_effect_x=settings["patch_area_effect_x"])
 
+sim.simulate(debug=True)
 
 # @ property
 # also initialise list as list()
