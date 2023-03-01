dd55302bc84848982224a87107b9a7a85888624b
Angus Wilson <1103557w@student.gla.ac.uk>
2023-03-01 16:17:53 +0000

    added things to debug

--- VSPOMs/simulator/ohno.py
+++ VSPOMs/simulator/ohno.py
@@ -7,14 +7,14 @@ a,b,c = parse_csv("demo.csv")
 # for patch in c:
 #     print(patch)
 d = c[:5]
-for p in d:
-    print(p)
+# for p in d:
+#     print(p)
 simulation = Simulator(d,
                        dispersal_alpha=float(b["dispersal_alpha"]),
                        area_exponent_b=float(b["area_exponent_b"]),
                        species_specific_constant_y=float(b["species_specific_constant_y"]),
                        species_specific_constant_u=float(b["species_specific_constant_u"]),
                        patch_area_effect_x=float(b["patch_area_effect_x"]),
-                       steps=100, replicates=1)
+                       steps=100, replicates=1, debug=True)
 
-simulation.simulate(debug=False)
+simulation.simulate()
