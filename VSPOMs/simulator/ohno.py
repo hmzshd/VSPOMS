d4eac5f04d5697660dbe3cccbb9c7c698be9c9e4
Angus Wilson <1103557w@student.gla.ac.uk>
2023-03-01 19:26:57 +0000

    more testing

--- VSPOMs/simulator/ohno.py
+++ VSPOMs/simulator/ohno.py
@@ -6,10 +6,7 @@ a,b,c = parse_csv("demo.csv")
 
 # for patch in c:
 #     print(patch)
-d = c[:5]
-# for p in d:
-#     print(p)
-simulation = Simulator(d,
+simulation = Simulator(c,
                        dispersal_alpha=float(b["dispersal_alpha"]),
                        area_exponent_b=float(b["area_exponent_b"]),
                        species_specific_constant_y=float(b["species_specific_constant_y"]),
