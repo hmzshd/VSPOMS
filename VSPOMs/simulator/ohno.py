8b36d18f8667650191ef78a1d1abefa924bd3b4d
Angus Wilson <1103557w@student.gla.ac.uk>
2023-03-01 15:08:20 +0000

    first run
    first run at fixing the issues, scaling the values down so that the minium x coord ends up being 0

--- VSPOMs/simulator/ohno.py
+++ VSPOMs/simulator/ohno.py
@@ -1,13 +1,14 @@
 from simulator import Simulator
 from parser import parse_csv
 from patch import Patch
-
+from patch_generator import generate_patch_list_random
 a,b,c = parse_csv("demo.csv")
 
 # for patch in c:
 #     print(patch)
-
-simulation = Simulator(c,
+d = c[:5]
+print(d)
+simulation = Simulator(d,
                        dispersal_alpha=float(b["dispersal_alpha"]),
                        area_exponent_b=float(b["area_exponent_b"]),
                        species_specific_constant_y=float(b["species_specific_constant_y"]),
@@ -15,4 +16,4 @@ simulation = Simulator(c,
                        patch_area_effect_x=float(b["patch_area_effect_x"]),
                        steps=100, replicates=1)
 
-simulation.simulate(debug=True)
+simulation.simulate(debug=False)
