8dc906bac74a1f55ecc3a0a9a76e4fe831964aab
Hamza Shahid <2578990s@student.gla.ac.uk>
2023-03-08 13:05:51 +0000

    Add work in progress post_patches test

--- VSPOMs/tests/test_views.py
+++ VSPOMs/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import Client, TestCase
+import json
 from VSPOMsApp.views import *
 
 class ViewsTestCase(TestCase):
@@ -10,6 +11,22 @@ class ViewsTestCase(TestCase):
     def test_generate_patch_list_random(self):
         patch_list = generate_patch_list_random(30)
         self.assertEqual(len(patch_list), 30)
+
+    def test_generate_patch_list_randomness(self):
+        patch_list = generate_patch_list_random(5)
+        patch_list2 = generate_patch_list_random(5)
+        self.assertNotEqual(patch_list, patch_list2)
+    
+    def test_generate_patch_list_properties(self):
+        patch_list = generate_patch_list_random(5)
+        for patch in patch_list:
+            self.assertIsInstance(patch.status, bool)
+            self.assertGreaterEqual(patch.x_coord, 0)
+            self.assertLessEqual(patch.x_coord, 25)
+            self.assertGreaterEqual(patch.y_coord, 0)
+            self.assertLessEqual(patch.y_coord, 25)
+            self.assertGreaterEqual(patch.radius, 0)
+            self.assertLessEqual(patch.radius, 5)
 
     def test_status_to_colour(self):
         statuses = [True, False, True]
@@ -50,3 +67,23 @@ class ViewsTestCase(TestCase):
     def test_index_plot(self):
         response = self.client.get("/")
         self.assertContains(response, "plot")
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_patches(self):
+        # create JSON data for the request
+        # WIP
+        request_data = {
+            "bokeh": {
+                "x": [1, 2, 3],
+                "y": [4, 5, 6],
+                "color": ["red", "green", "blue"],
+                "size": [1.0, 2.0, 3.0]
+            },
+            "dispersal_kernel": 0.5,
+            "connectivity": 0.6,
+            "colonization_probability": 0.7,
+            "patch_extinction_probability_u": 0.8,
+            "patch_extinction_probability_x": 0.9
+        }
